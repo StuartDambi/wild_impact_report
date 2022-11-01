65f82d198cca82351730dcebb198f37f71b46d8d
stuartdambi <stuartdambi@gmail.com>
2022-11-01 09:51:45 +0300

    add timeline

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -135,6 +135,196 @@ const Main = () => {
 
                             </div>
                         </section>
+
+
+                        {/* Timeline Start */}
+                        <section className="timeline_area section_padding_130">
+                            <div className="container">
+                                <div className="row justify-content-center">
+                                    <div className="col-12 col-sm-8 col-lg-6">
+                                        {/* <!-- Section Heading--> */}
+                                        <div className="section_heading text-center">
+                                            <h6>Timeline</h6>
+                                            <h3>A nice heading for the coffee journey</h3>
+                                            <div className="line"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        {/* <!-- Timeline Area--> */}
+                                        <div className="apland-timeline-area">
+                                            {/* <!-- Single Timeline Content--> */}
+                                            <div className="single-timeline-area">
+                                                <div className="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style={{ visibility: "visible", animationDelay: "0.1s", animationName: "fadeInLeft" }}>
+                                                    <p>November 2021</p>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style={{ visibility: "visible", animationDelay: "0.3s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-address-card" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Harvesting</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    {/* <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style={{ visibility: "visible", animationDelay: "0.5s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-archive" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Fixed bug</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.7s" style={{ visibility: "visible", animationDelay: "0.7s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-address-book" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Reach 1k Users</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div> */}
+                                                </div>
+                                            </div>
+                                            {/* <!-- Single Timeline Content--> */}
+                                            <div className="single-timeline-area">
+                                                <div className="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style={{ visibility: "visible", animationDelay: "0.1s", animationName: "fadeInLeft" }}>
+                                                    <p>March 2022</p>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style={{ visibility: "visible", animationDelay: "0.3s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-briefcase" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Processing</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    {/* <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style={{ visibility: "visible", animationDelay: "0.5s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-desktop" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Fixed bug</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div> */}
+                                                </div>
+                                            </div>
+                                            {/* <!-- Single Timeline Content--> */}
+                                            <div className="single-timeline-area">
+                                                <div className="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style={{ visibility: "visible", animationDelay: "0.1s", animationName: "fadeInLeft" }}>
+                                                    <p>April 2022</p>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style={{ visibility: "visible", animationDelay: "0.3s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-id-card" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Warehouse</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    {/* <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style={{ visibility: "visible", animationDelay: "0.5s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-desktop" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Fixed bug</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.7s" style={{ visibility: "visible", animationDelay: "0.7s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-picture-o" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Reach 500 Users</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div> */}
+                                                </div>
+                                            </div>
+                                            {/* <!-- Single Timeline Content--> */}
+                                            <div className="single-timeline-area">
+                                                <div className="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style={{ visibility: "visible", animationDelay: "0.1s", animationName: "fadeInLeft" }}>
+                                                    <p>25th October 2022</p>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style={{ visibility: "visible", animationDelay: "0.3s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-id-card" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Coffee Roasting</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    {/* <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style={{ visibility: "visible", animationDelay: "0.5s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-desktop" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Fixed bug</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.7s" style={{ visibility: "visible", animationDelay: "0.7s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-picture-o" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Reach 500 Users</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div> */}
+                                                </div>
+                                            </div>
+                                            {/* <!-- Single Timeline Content--> */}
+                                            <div className="single-timeline-area">
+                                                <div className="timeline-date wow fadeInLeft" data-wow-delay="0.1s" style={{ visibility: "visible", animationDelay: "0.1s", animationName: "fadeInLeft" }}>
+                                                    <p>26th October 2022</p>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style={{ visibility: "visible", animationDelay: "0.3s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-id-card" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Coffee Exporting</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    {/* <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.5s" style={{ visibility: "visible", animationDelay: "0.5s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-desktop" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Fixed bug</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-12 col-md-6 col-lg-4">
+                                                        <div className="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.7s" style={{ visibility: "visible", animationDelay: "0.7s", animationName: "fadeInLeft" }}>
+                                                            <div className="timeline-icon"><i className="fa fa-picture-o" aria-hidden="true"></i></div>
+                                                            <div className="timeline-text">
+                                                                <h6>Reach 500 Users</h6>
+                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div> */}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
